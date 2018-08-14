b37bba66ad9a1eb032b5ebdfdd5fd96bfa86303e
mariouvega <mario.vega014@gmail.com>
2018-08-14 14:10:30 -0400

    was able to get name and price in an e-commerce site

--- scrapeThree.js
+++ scrapeThree.js
@@ -2,12 +2,12 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const fs = require('fs');
-const writeStream = fs.createWriteStream('post.csv');
+const writeStream = fs.createWriteStream('postTwo.csv');
 
 // WRITE HEADERS
-writeStream.write(`Title,Link,Date \n`);
+// writeStream.write(`Name,Price,Rating \n`);
 
-request('https://www.webscraper.io/test-sites/e-commerce/allinone', (error, response, html) => {
+request('https://www.webscraper.io/test-sites/e-commerce/allinone/computers/tablets', (error, response, html) => {
 	
 	if (!error && response.statusCode == 200) {
 		const $ = cheerio.load(html)
@@ -17,11 +17,35 @@ request('https://www.webscraper.io/test-sites/e-commerce/allinone', (error, resp
 		// console.log(jumbotron.text())
 
 		const heading = jumbotron.find('h1').text()
-		const headingTwo = jumbotron.find('h1').next().text()
 		const paragraph = jumbotron.children('p').text()
-		console.log(heading)
-		console.log(headingTwo)
-		console.log(paragraph)
+		// console.log(heading)
+		// console.log(paragraph)
+
+
+
+		$('.thumbnail').each((i,el) => {
+			const name = $(el).find('.title').text()
+			const price = $(el).find('.price').text().replace(/\$/, '')
+			const rating = $(el).find('p').attr('rating')
+			console.log(name, price, rating)
+
+			// WRITE ROW TO CSV
+			// writeStream.write(`${name}, ${price}, ${rating} \n`)
+		})
+			console.log('Scraping Complete...')
+
+
+
+		// $('.title').each((i,el) => {
+		// 	const name = $(el).text()
+		// 	const link = $(el).attr('href')
+		// 	console.log(name, link)
+		// })
+
+		// $('.price').each((i,el) => {
+		// 	const price = $(el).text().replace(/\$/, '')
+		// 	console.log(price)
+		// })
 
 		// 	const title = $(el).find('.post-title').text().replace(/\s\s+/g, '')
 		// 	const link = $(el).find('a').attr('href')
