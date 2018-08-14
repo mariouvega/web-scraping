0882dd5869cf83fdd6dd5d1ee709f85bb817e0dd
mariouvega <mario.vega014@gmail.com>
2018-08-13 23:33:15 -0400

    created a request and printed it out to the terminal

--- scrape.js
+++ scrape.js
@@ -1,4 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('http://codedemos.com/sampleblog/');
+request('http://codedemos.com/sampleblog', (error, response, html) => {
+	if (!error && response.statusCode == 200) {
+		console.log(html)
+	}
+});
